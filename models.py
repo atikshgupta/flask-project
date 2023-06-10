4e74d8c5833d5306bb0f7f00e1d091ee873ad671
Atiksh Gupta <atiksh.gupta9876@gmail.com>
2023-06-10 19:33:22 +0800

    doc_texts changed to doc_dicts, multiple PDFs allowed

--- models.py
+++ models.py
@@ -12,10 +12,12 @@ from pypdf import PdfReader
 response_page = Blueprint("response_page", __name__)
 
 
-def get_documents_text():
+def get_documents_dict():
     file_ext_dict = {}
+    file_names = []
     uploaded_files = os.listdir("/Users/atikshgupta/Desktop/flask-project/uploaded_files")
     for file_name in uploaded_files:
+        file_names.append(file_name)
         if file_name not in file_ext_dict:
             file_ext_dict[file_name] = file_name.split('.')[-1]
     
@@ -27,21 +29,34 @@ def get_documents_text():
             pages_text = [page.extract_text() for page in pages]
             full_doc_text = " ".join(pages_text)
             all_docs.append(full_doc_text)
-
     
+    file_names_and_contents = {}
+    for i in range(len(file_names)):
+        file_names_and_contents[uploaded_files[i]] = all_docs[i]
     
+    print(file_names_and_contents)
+
+    return file_names_and_contents # only works for 1 pdf atm
+
+
+def chatmode_process(prompt):
+    all_docs_in_a_dict = get_documents_dict()
+    docs_names = list(all_docs_in_a_dict.keys())
+    doc_dict = {"role": "user", "content": f"Document 1 — name: {docs_names[0]}, content: {all_docs_in_a_dict[docs_names[0]]}"}
+    doc_dict_list = [doc_dict]
+    for i in range(1, len(docs_names)):
+        new = doc_dict.copy()
+        new["content"] = f"Document {i} — name: {docs_names[i]}, content: {all_docs_in_a_dict[docs_names[i]]}"
+        doc_dict_list.append(new)
     
+    messages = [d for d in doc_dict_list]
+    messages.insert(0, {"role": "system", "content": f"You are to read the following documents, I will ask questions about them. Be very terse and short in your responses"})
 
-    return all_docs[0] # only works for 1 pdf atm
+    print(messages)
 
-
-def chatmode_process(document_text, prompt):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": f"You are to read the following document, I will ask questions about it: {document_text}. Be very terse and short in your responses"},
-            {"role": "user", "content": f"{prompt}"}
-        ]
+        messages=messages
     )
     return response["choices"][0]["message"]["content"]
 
@@ -60,13 +75,12 @@ def bot_response():
     if request.method == "POST":
         if "user-prompt" in request.form:
             conversation.append(request.form.get("user-prompt"))
-            conversation.append(chatmode_process(get_documents_text(), request.form.get("user-prompt")))
+            conversation.append(chatmode_process(request.form.get("user-prompt")))
         elif "right-addfile" in request.form:
             return redirect(url_for("upload"))
         elif "right-chatmode" in request.form:
             return render_template("QApage.html", conversation=conversation, loaded_documents=loaded_documents, mode="chat mode")
         elif "right-docsmode" in request.form:
-            a = get_documents_text()
             return render_template("QApage.html", conversation=conversation, loaded_documents=loaded_documents, mode="docs mode")
 
     return render_template("QApage.html", conversation=conversation, loaded_documents=loaded_documents, mode="docs mode")
